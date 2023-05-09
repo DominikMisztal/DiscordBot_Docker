c7f2b7429aa07dcbd386e826c06466e833224bc7
DMisztal <dominik.misztal@onet.eu>
2023-05-09 19:06:17 +0200

    Bot connection

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-const Discord = require('discord.js');
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 
-const discordClient = new Discord.Client();
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+
+client.login(token);
 
